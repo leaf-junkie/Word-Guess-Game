b8efc776ec630ece161f384fcded9e0523f5862e
leaf-junkie <ragsdale.jar@gmail.com>
2018-12-31 02:07:32 -0500

    major update alert
    added a drawLetters function
    updated some variable names
    added checkIfPlantHasLetter function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,64 @@
+// Select random plant name
+const plants =                     // Word list
+    [   
+        'monstera', 
+        'pothos', 
+        'string of hearts',
+        'english ivy',
+        'devils ivy',
+        'rubber plant',
+        'zz plant',
+        'fiddle leaf fig',
+        'succulent',
+        'maidenhair fern',
+        'jade plant',
+        'heartleaf philodendron',
+        'cactus',
+        'prayer plant',
+        'aloe',
+        'pinstripe calathea',
+        'bird of paradise',
+        'pilea peperomioides',
+        'rattlesnake plant',
+        'sansevieria',
+        'air plant',
+        'tillandsia',
+        'staghorn fern',
+        'peperomia',
+        'string of pearls',
+        'burros tail' 
+    ];
+
+// Use Math.floor to round the random number down to the nearest whole number
+let randomIndex = Math.floor(Math.random() * plants.length);
+let thePlant = plants[randomIndex];
+// Print these values to the console
+console.log(randomIndex);
+console.log(thePlant);
+
+// Create empty list for randomly selected plant and push to screen
+plantLetters = [];
+for (var letter in thePlant) {
+    var letterObject = {
+        value: letter,
+        visible: false
+    }
+    plantLetters.push(letter);
+}
+
 // Create event listener for when a key is pressed
 document.onkeyup = keyUpHandler
 
-// Generate random plant name
-const plants = ['monstera', 
-            'pothos', 
-            'string of hearts',
-            'english ivy',
-            'devils ivy',
-            'rubber plant',
-            'zz plant',
-            'fiddle leaf fig',
-            'succulent',
-            'maidenhair fern',
-            'jade plant',
-            'heartleaf philodendron',
-            'cactus',
-            'prayer plant',
-            'aloe',
-            'pinstripe calathea',
-            'bird of paradise',
-            'pilea peperomioides',
-            'rattlesnake plant',
-            'sansevieria',
-            'air plant',
-            'tillandsia',
-            'staghorn fern',
-            'peperomia',
-            'string of pearls',
-            'burros tail' 
-        ]
-
-const plantSets = []
-plants.forEach(plant=> {
-    let stringArray = plant.split('') // ['f', 'o', 'o']
-    plantSets.append(
-        new Set(stringArray)
-    )
-})
-
-let randomIndex = Math.floor(Math.random() * plants.length);
-let thePlant = thePlant[randomIndex];
-    
 // Move guessed letters into an array
 let letters = 'abcdefghijklmnopqrstuvwxyz';
 letters = letters.toUpperCase();
 let guessedLetters = [];
 
-console.log(randomIndex);
-console.log(thePlant);
-
+// The number of guesses the player has
 let guessesRemaining = 10;
 document.getElementById('guessesRemaining').innerHTML = guessesRemaining;
 
-// Beginning of game logic
+// This is the game logic
 
 // Get input from user when a key is pressed
 function keyUpHandler(event) {
@@ -60,7 +66,7 @@ function keyUpHandler(event) {
     if (!letters.includes(event.key.toUpperCase())) {
         return;
     }
-
+    
     // Ignore redundant input from user
     // On key press, check if the letter has already been pressed
     if (guessedLetters.includes(event.key)) {
@@ -69,23 +75,23 @@ function keyUpHandler(event) {
     } else {
         // If it has been pressed, return false
         guessedLetters.push(event.key);
-
-        // Display number of guesses remaining under 'Your Guesses Thus Far'
-        document.getElementById('guessedLetters').innerHTML = guessedLetters.join(', ');
     }
-
+    
     // Show pressed keys in console
-    console.log(guessedLetters);
-
-    // Check if key is part of the plant
-    if (thePlant.includes(event.key.toUpperCase() === letters.toUpperCase())) {
-        document.getele
-        // If it is the magic letter, win (call a win function)
-        win();
+    console.log('Key pressed: ' + guessedLetters);
+    
+    // Display the letters that have been guessed
+    document.getElementById('guessedLetters').innerHTML = guessedLetters.join(', ');
+    
+    // Check if pressed letter is included in name of current plant 
+    if (checkIfPlantHasLetter(event.key)) {
+        // had the letter
     } else {
-        // If not, decrement guess count
-        guessesRemaining -= 1;
+        // did not have the letter
     }
+    // draw the letters
+    drawLetters();
+    
 }
 
 // When hint button is clicked, show relevant image
@@ -101,6 +107,10 @@ if (guessesRemaining === 0) {
     lose();
 }
 
+// These are the functions
+
+
+// Invoke a win
 function win() {
     console.log('You win!');
     // Display number of wins in browser
@@ -113,6 +123,7 @@ function win() {
     winScreen.style.visibility = 'visible';
 }
 
+// Invoke a loss
 function lose() {
     console.log('You lose!');
     // Display number of losses in browser
@@ -125,15 +136,16 @@ function lose() {
     loseScreen.style.visibility = 'visible';
 }
 
+// Reset plant after win or loss
 function reset() {
     // Reset guesses remaining
     guessesRemaining = 10;
     document.getElementById('guessesRemaining').innerHTML = guessesRemaining;
-
+    
     // Empty guessed letters array
     guessedLetters = [];
     document.getElementById('guessedLetters').innerHTML = guessedLetters.join(', ');
-
+    
     // Select new magic letter
     randomIndex = Math.floor(Math.random() * letters.length);
     magicLetter = letters[randomIndex];
@@ -142,4 +154,35 @@ function reset() {
     // Make win screen and lose screen hidden on reset
     winScreen.style.visibility = 'hidden';
     loseScreen.style.visibility = 'hidden';
+}
+
+// Check if plant contains a letter and make correctly guessed letters visible
+function checkIfPlantHasLetter(guessedLetter) {
+    var hasLetter = false;
+
+    // Loop over letters in plantLetters (for letter in plantLetters)
+    for (var letter in plantLetters) {
+        // If the current letter is the guessedLetter, set visible to true, and hasLetter to true
+        if (letter.value === guessedLetter) {
+            letter.visible = true;
+            hasLetter = true;
+        }
+    }
+
+    return hasLetter;
+}
+
+// Draw letters onto the screen
+function drawLetters() {
+    var html = '';
+    for (var letter in plantLetters) {
+        if (letter.visible) {
+            html += letter.value;
+        }
+        else {
+            // If not, draw an underscore in place of the letter
+            html += '_';
+        }
+    }
+    document.getElementsByClassName('the-plant').innerHTML = html;
 }
